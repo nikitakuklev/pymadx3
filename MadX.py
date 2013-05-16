f9a50c44f150eeb7f0bc5f3ef30140b227b0dd36
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2013-05-16 22:12:07 +0100

    Basic optics classes

--- MadX.py
+++ MadX.py
@@ -1,6 +1,15 @@
-import pylab as pl
+import pylab as _pl
+
 class Tfs :
+    '''class Tfs
+       Tape file system data object  
+       Tape.header : is a dictionary of header keys and values 
+       Tape.dataName : column names 
+       Tape.dataType : tfs column data type 
+       Tape.data     : dictionary of data'''
+
     def __init__(self, fileName) :
+        '''fileName is tfs file'''        
         self.fileName = fileName
         self.header   = dict() 
         self.dataName = list()
@@ -10,6 +19,7 @@ class Tfs :
         self.readFile(fileName) 
 
     def readFile(self, fileName) :
+        '''read tfs file'''
         f = open(fileName,'r')
         
         # loop over all lines in file
@@ -55,4 +65,46 @@ class Tfs :
                         
         # Convert to array
         for v in self.dataName : 
-            self.data[v] = pl.array(self.data[v])            
+            self.data[v] = _pl.array(self.data[v])            
+
+class Common : 
+    '''class Common 
+       Class to extract information from madx output    
+    '''
+    def __init__(self) :
+        '''Requires no arguments'''
+        self._ind = _pl.arange(0,len(self.data[self.data.keys()[0]]),1)
+
+    def findRowString(self, name, column = 'NAME') : 
+        '''Find indicies of name in column, 
+           eg Common.findRowString('MQXA.1R1..1','NAME') 
+           eg Common.findRowString('MULTIPOLE','KEYWORD')''' 
+        return self._ind[self.data[column] == name]
+
+    def getRowIndicies(self, ind, column = 'NAME') : 
+        '''Find indicies of name in column, 
+           eg i = Common.findRowString('MULTIPOLE','KEYWORD')
+           eg n = Common.getRowIndices(i,'NAME')'''
+        return self.data[column][ind]
+
+    def getUnique(self, column) : 
+        '''Find unique values a column 
+           eg Common.getUnique('KEYWORD') '''
+        return _pl.unique(self.data[column])
+
+class Twiss(Common,Tfs) : 
+    '''class Twiss(Common,Tfs)
+       Twiss data storage object
+       Twiss.data is a dictionary containing numpy.arrays of the twiss output of madX'''
+
+    def __init__(self, fileName) : 
+        '''fileName is a madX twiss output file
+           eg t = MadX.Twiss("./test/LHC/HL/madx_optics_beta1.5m/twiss_b1.data.thin")'''
+        
+        Tfs.__init__(self,fileName)
+        Common.__init__(self)
+    
+class Survey : 
+    def __init__(self, fileName) :
+        pass
+
