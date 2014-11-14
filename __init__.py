361fa499428d0fc34cfe0419c82e53d6145fbb6d
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2014-11-14 09:02:46 +0000

    tidy of repository - removal of older code that's unused

--- __init__.py
+++ __init__.py
@@ -1,7 +1,13 @@
-import MadX
-from Survey import Survey 
-import Convert
-import Mad8
+"""
+pymadx - Madx TFS file parser
+
+Authors: L. Nevay, S. Boogert
+2014 11 14
+
+Tfs           - a loader class
+Visualisation - visualisation of the loaded lattice
+
+"""
 
 from Tfs import Tfs
 from Visualisation import Visualisation
