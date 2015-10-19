nikitakuklev/pymadx3

commit 3220bf7bcd033ffd3e97afce14ac6871a109272b

Changed it

Plot.py
@@ -105,8 +105,8 @@ def PlotTfsBeta(tfsfile, title='',outputfilename=None):
 def AddMachineLatticeToFigure(figure,tfsfile):
     tfs = _CheckItsTfs(tfsfile) #load the machine description
 
-    axs = figure.get_axes() #get the existing graph
-    axoptics = axs[0]  #get the only presumed axes from the figure
+    axs = figure.get_axes() # get the existing graph
+    axoptics = axs[0]       # get the only presumed axes from the figure
 
     #adjust existing plot to make way for machine lattice
     #iterate over axes incase there's dual plots
@@ -180,7 +180,7 @@ def _DrawMachineLattice(axesinstance,pymadxtfsobject):
     ax.set_ylim(-0.5,0.5)
  
     # loop over elements and Draw on beamline
-    for element in tfs:
+    for element in c :
         kw = element['KEYWORD']
         if kw == 'QUADRUPOLE': 
             DrawQuad(element)

commit 59e28c8d6b7ab4d71048da9cac4c6b60bcf06d16

fix bug introduced in commit 3220bf7 that broke plotting - not sure why this was changed.

Plot.py
@@ -180,7 +180,7 @@ def _DrawMachineLattice(axesinstance,pymadxtfsobject):
     ax.set_ylim(-0.5,0.5)
  
     # loop over elements and Draw on beamline
-    for element in c :
+    for element in tfs:
         kw = element['KEYWORD']
         if kw == 'QUADRUPOLE': 
             DrawQuad(element)