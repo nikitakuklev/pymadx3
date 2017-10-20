50f90bb770b847e0cecf40802d7035b7c0239102
swalker92 <zwap082@live.rhul.ac.uk>
2017-10-20 16:15:39 +0100

    Working setuptools setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-   name='pymadx',
-   version='1.0',
-   description='BDSIM python helper package',
-   author='JAI@RHUL',
-   author_email='stewart.boogert@rhul.ac.uk',
-#   py_modules = ['__init__','Beam','Builder','Constants','Data','Gmad','Joinhistograms',
-#                 'ModelProcessing','Options','Plot','Run','Visualisation','XSecBias','_General'],
-   package_dir = {'pymadx': './'},
-   packages=['pymadx'],
+    name='pymadx',
+    version='0.9',
+    packages=find_packages(exclude=["docs", "tests", "obsolete"]),
+    # Not sure how strict these need to be...
+    install_requires=["matplotlib >= 2.1.0",
+                      "numpy >= 1.13"],
+    # Some version of python2.7
+    python_requires="==2.7.*",
+
+    author='JAI@RHUL',
+    author_email='stewart.boogert@rhul.ac.uk',
+    description="Write MADX models and load MADX output.",
+    url='https://bitbucket.org/jairhul/pymadx/'
 )
