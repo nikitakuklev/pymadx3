1827e9eed870929c07eacb0d3ee127613b8e6233
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-03-28 16:59:20 +0200

    Allow saving of madx comparison as usual way and set default figure size.  Note figsize cannot be supplied as a kwarg to both the figure and the plotting - nothing can really!

--- pymadx/Compare/_MadxMadxComparison.py
+++ pymadx/Compare/_MadxMadxComparison.py
@@ -6,9 +6,10 @@ Functions for plotting the individual optical functions, and an
 eighth, helper function ''compare_all_optics``, to plot display all
 seven in one go.
 """
-
-import os.path as _path
+import datetime as _datetime
 import matplotlib.pyplot as _plt
+from matplotlib.backends.backend_pdf import PdfPages as _PdfPages
+import os.path as _path
 
 import pymadx.Data as _Data
 import pymadx.Plot as _Plot
@@ -65,7 +66,7 @@ def _make_plotter(plot_info_tuples, x_label, y_label, title):
         first, first_name = _parse_tfs_input(first, first_name)
         second, second_name = _parse_tfs_input(second, second_name)
 
-        plot = _plt.figure(title, **kwargs)
+        plot = _plt.figure(title, figsize=(9,5), **kwargs)
         # Loop over the variables in plot_info_tuples and draw the plots.
         for var, legend_name in plot_info_tuples:
             _plt.plot(first.GetColumn('S'), first.GetColumn(var),
@@ -94,21 +95,34 @@ PlotMean   = _make_plotter(_MEAN,    "S / m", r"$\bar{x}, \bar{y}$ / m", "Mean")
 
 
 def MADXVsMADX(first, second, first_name=None,
-               second_name=None, **kwargs):
+               second_name=None, saveAll=True, 
+               outputFileName=None, **kwargs):
     """
     Display all the optical function plots for the two input optics files.
     """
+    figures = [
     PlotBeta(first, second, first_name=first_name,
-             second_name=second_name, **kwargs)
+             second_name=second_name, **kwargs),
     PlotAlpha(first, second, first_name=first_name,
-              second_name=second_name, **kwargs)
+              second_name=second_name, **kwargs),
     PlotDisp(first, second, first_name=first_name,
-             second_name=second_name, **kwargs)
+             second_name=second_name, **kwargs),
     PlotDispP(first, second, first_name=first_name,
-              second_name=second_name, **kwargs)
+              second_name=second_name, **kwargs),
     PlotSigma(first, second, first_name=first_name,
-              second_name=second_name, **kwargs)
+              second_name=second_name, **kwargs),
     PlotSigmaP(first, second, first_name=first_name,
-               second_name=second_name, **kwargs)
+               second_name=second_name, **kwargs),
     PlotMean(first, second, first_name=first_name,
-             second_name=second_name, **kwargs)
+             second_name=second_name, **kwargs),
+    ]
+
+    if saveAll:
+        output_filename = "optics-report.pdf"
+        with _PdfPages(output_filename) as pdf:
+            for figure in figures:
+                pdf.savefig(figure)
+            d = pdf.infodict()
+            d['Title'] = "{} VS {} Optical Comparison".format(first_name, second_name)
+            d['CreationDate'] = _datetime.datetime.today()
+        print "Written ", output_filename
