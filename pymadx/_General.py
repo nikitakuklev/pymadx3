797ec2d678c3c906c2a7f4396c1cc0e62c70b3a7
swalker92 <zwap082@live.rhul.ac.uk>
2018-07-11 16:05:55 +0100

    ProcessSixTrackAper function for correct interpolation

--- pymadx/_General.py
+++ pymadx/_General.py
@@ -61,6 +61,46 @@ def IndexOfElement(tfsinstance,markername):
         print 'Unknown element name'
     return i
 
+def ProcessSixTrackAper(item):
+    """Processes the aperture so that it can be used for
+    interpolation.  E.g. the bounding rectangle should tightly wrap
+    the contained ellipse.  This has no impact on the aperture,
+    itself, but the resulting linearly interpolated apertures will
+    be more correct."""
+    # Note this is a reimplementation of the logic from
+    # BeamLossPattern.  I understand 3/4 of the following steps.  The
+    # step I don't understand is why, if the rectangle is inside the
+    # ellipse then, the ellipse must be increased in size by sqrt(2)
+    # (doubling of the area).  I do it anyway.
+    a1 = item["APER_1"]
+    a2 = item["APER_2"]
+    a3 = item["APER_3"]
+    a4 = item["APER_4"]
+
+    # if the rectangle is bigger than the ellipse bring it down to
+    # size to tightly wrap the contained ellipse.
+    if a1 > a3 and a3 != 0:
+        a1 = a3
+    # This is saying the same as above, but in different dimension.
+    if a2 > a4 and a4 > 0:
+        a2 = a4
+    # if the corner of the rectangle is inside the ellipse then make
+    # the ellipse bigger so that it is definitely big enough.  I have
+    # no idea why this step is here other than to copy others.
+    if a1 != 0 and a2 != 0 and a3 != 0 and (a1/a3)**2 + (a2/a4)**2 < 0.99999999:
+        a3 = a1 * math.sqrt(2)
+        a4 = a2 * math.sqrt(2)
+    # if a4 is negative or >0.5 then it must be a rectangle (because
+    # a4 of those values can only reasonably be an angle). and if
+    # it's a rectangle then a3 must be 0.
+    if a4 < 0 or a4 > 0.5:
+        a3 = 0
+
+    item["APER_1"] = a1
+    item["APER_2"] = a2
+    item["APER_3"] = a3
+    item["APER_4"] = a4
+
 def GetSixTrackAperType(a1, a2, a3, a4):
     """Return the Aperture type given the four aperture paramters.
     This is required because SixTrack aperture description files do
@@ -73,44 +113,15 @@ def GetSixTrackAperType(a1, a2, a3, a4):
     # a3 = ellipse x semi-axis  (i.e. x)
     # a4 = ellipse y semi-axis  (i.e. y)
 
-        """Return the Aperture type given the four aperture paramters.
-    This is required because SixTrack aperture description files do
-    not explictily state the aperture type.  It is instead encoded in
-    the aperture parameters."""
-
-    # Rectellipse is the intersection of an ellipse with a concentric rectangle.
-    # a1 = rectangle half-width (i.e. x)
-    # a2 = rectangle half-height (i.e. y)
-    # a3 = ellipse x semi-axis  (i.e. x)
-    # a4 = ellipse y semi-axis  (i.e. y)
-
     # In general all the apertures are rectellipses except for two
     # cases:
 
-    # a3=0 denotes st a rectangle (since a3=0 = empty set and thus has
+    # a3=0 denotes a rectangle (since a3 = 0 = empty set and thus has
     # no meaning for a rectellipse).
 
     # a1 == 0 and a2 == 0 apparently denotes a RACETRACK, but I'm not
     # sure BeamLossPattern supports this (even though there is the source
     # for it).
-
-    # # if the rectangle is bigger than the ellipse bring it down to size.
-    # if a1 > a3 and a3 != 0:
-    #     a1 = a3
-    # # This is saying teh same as above, but in different dimension.
-    # if a2 > a4 and a4 > 0:
-    #     a2 = a4
-    # # if the corned of the rectangle is inside the ellipse then make
-    # # the ellipse bigger so that it is definitely big enough.  I have
-    # # no idea why this step is here other than to copy others.
-    # if a1 != 0 and a2 != 0 and a3 != 0 and (a1/a3)**2 + (a2/a4)**2 < 0.99999999:
-    #     a3 = a1 * math.sqrt(2)
-    #     a4 = a1 * math.sqrt(2)
-    # # if a4 is negative or >0.5 then it must be a rectangle (because
-    # # a4 of those values can only reasonably be an angle). and if
-    # # it's a rectangle then a3 must be 0.
-    # if a4 < 0 or a4 > 0.5:
-    #     a3 = 0
 
     # Rectellipse with the parameters which result in an ellipse
     if a1 == a3 and a2 == a4:
