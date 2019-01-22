18263dd3482efd7f261c32cd1960b994fbadd048
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-01-22 21:26:08 +0100

    Fix python highlighting colours given sphinx 1.8.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,7 +75,8 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+#pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
